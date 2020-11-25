861e22d1ec493e00403084e2100eb03c6329356c
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-25 20:14:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -10,7 +10,7 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   
-  array.count {|element| friend.counter >= 5 }
+  #array.count {|element| friend.counter >= 5 }
   
    array.count do |element|
     element = ""
